094f25f019a0d225a096644a3233f6f52a03b59f
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2022-11-15 01:12:10 +0700

    add search product

---
+++ DATA.sql
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+USE SaleManagement
+GO
+
+INSERT INTO Member
+VALUES(1,'member1@gmail.com','FPT Company','TP HCM','VN', '123'),
+(2,'member2@gmail.com','Vietinbank Company','TP HCM','VN', '123'),
+(3,'member3@gmail.com','Vinamilk Company','TP HCM','VN', '123'),
+(4,'member4@gmail.com','Habeco Company','TP HCM','VN', '123'),
+(5,'member5@gmail.com','MobiFone Company','TP HCM','VN', '123')
+
+INSERT INTO Product
+VALUES(1,1,'Product 1','4,3kg',10000000,100),
+(2,1,'Product 2','2,1kg',20000000,100),
+(3,1,'Product 3','1,3kg',18000,100),
+(4,1,'Product 4','100g',4000,100),
+=======
+USE SaleManagement
+GO
+
+INSERT INTO Member
+VALUES(1,'member1@gmail.com','FPT Company','TP HCM','VN', '123'),
+(2,'member2@gmail.com','Vietinbank Company','TP HCM','VN', '123'),
+(3,'member3@gmail.com','Vinamilk Company','TP HCM','VN', '123'),
+(4,'member4@gmail.com','Habeco Company','TP HCM','VN', '123'),
+(5,'member5@gmail.com','MobiFone Company','TP HCM','VN', '123')
+
+INSERT INTO Product
+VALUES(1,1,'Product 1','4,3kg',10000000,100),
+(2,1,'Product 2','2,1kg',20000000,100),
+(3,1,'Product 3','1,3kg',18000,100),
+(4,1,'Product 4','100g',4000,100),
+>>>>>>> 74ca44e (add search product)
+(5,1,'Product 5','300kg',5000,100)
---
+++ README.md
@@ -0,0 +1,1 @@
+# PRN_Assignment2
